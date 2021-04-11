3c9a26138493b1f0b81e47223a8a4faab9a1216d
Paul Cirstea <paul.cirstea@etu.univ-grenoble-alpes.fr>
2021-04-11 18:53:59 +0200

    implem jdl

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'blog',
+        loadChildren: () => import('./blog/blog.module').then(m => m.MyappBlogModule),
+      },
+      {
+        path: 'post',
+        loadChildren: () => import('./post/post.module').then(m => m.MyappPostModule),
+      },
+      {
+        path: 'tag',
+        loadChildren: () => import('./tag/tag.module').then(m => m.MyappTagModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
